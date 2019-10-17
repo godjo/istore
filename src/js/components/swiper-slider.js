cdcb2bc6cfe4b2f3fe3252bdc2dcb09f9ea6b73e
godjo <aal940722@gmail.com>
2019-10-17 15:10:24 +0300

    feature: adds features

--- src/js/components/swiper-slider.js
+++ src/js/components/swiper-slider.js
@@ -1,6 +1,26 @@
-var swiper = new Swiper('.main-banner__container', {
+var swiperBanner = new Swiper('.main-banner__container', {
   navigation: {
     nextEl: '.main-banner__button--next',
     prevEl: '.main-banner__button--prev',
+  },
+});
+
+var swiperFeatures = new Swiper('.main-features__container', {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    960: {
+      slidesPerView: 2,
+    },
+    1300: {
+      slidesPerView: 3,
+    },
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
   },
 });
