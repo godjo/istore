4cd954cede53b988b866a0f9c91aa99f9523baa7
Mac <mac@MacBook-Air-Macbook-5.local>
2019-11-19 18:28:50 +0300

    feature: adds new banners

--- src/js/components/swiper-slider.js
+++ src/js/components/swiper-slider.js
@@ -1,11 +1,11 @@
 var swiperBanner = new Swiper('.main-banner__container', {
+  loop: true,
   navigation: {
     nextEl: '.main-banner__button--next',
     prevEl: '.main-banner__button--prev',
   },
   breakpoints: {
     1250: {
-      loop: true,
       spaceBetween: 10,
       slidesPerView: 'auto',
       centeredSlides: true,
